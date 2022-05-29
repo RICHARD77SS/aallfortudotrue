3105fb7986d7736a5471b7c79598dafa8d7a880b
RICHARD77SS <92746991+RICHARD77SS@users.noreply.github.com>
2022-05-29 15:20:20 -0300

    Script todo list, funçao de criar task, gerar id e atualizar a tela

---
+++ home/Script/todolist.js
@@ -0,0 +1,41 @@
+var tasks = [];
+
+function idGenerator() {
+  var timeStamp = new Date();
+
+  var id =
+    timeStamp.getHours().toString() +
+    timeStamp.getMinutes().toString() +
+    timeStamp.getSeconds().toString() +
+    timeStamp.getMilliseconds().toString();
+  
+  return id;
+}
+
+function createTask() {
+  var taskDescription = document.getElementById("newTask").value
+
+  var task = {
+    id: idGenerator(),
+    data: {
+      description: taskDescription
+    }
+  };
+
+  tasks.push(task);
+
+  updateScreen()
+}
+
+function updateScreen() {
+  var list = "<ul>";
+
+  tasks.forEach(task => {
+    list += "<li id-data=" + task.id + ">" + task.data.description + "</li>";
+  });
+
+  list += "</ul>";
+
+  document.getElementById("listTodo").innerHTML = list;
+  document.getElementById("newTask").value = "";
+}
